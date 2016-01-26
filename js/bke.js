dc42a3bb5818cf6327c88d7da3df8794af9d6f2c
danique646 <danique-bruintjes@hotmail.com>
2016-01-26 12:49:54 +0100

    Completed

--- js/bke.js
+++ js/bke.js
@@ -10,6 +10,9 @@ var start_spel= 0;
 var imagelist = ["", "", "", "", "", "", "", "", ""];
 var playerimage;
 var playernumber;
+var player1score;
+var player2score;
+var gamerounds;
 
 for(loop = 0; loop < 9; loop++){
     imagelist[loop] = document.getElementsByTagName('img')[loop];
@@ -32,6 +35,11 @@ window.onload = function () {
 
     playerimage = document.querySelector('#game-info img');
     playernumber = document.querySelector('#game-info table tr td:last-child');
+
+    player1score = document.querySelector('.rounds-info tr td:last-child ');
+    player2score = document.querySelectorAll('.rounds-info tr td')[3];
+
+    gamerounds = document.querySelector('.rounds-info tr:last-child td:last-child');
 };
 
 function btnstartreset(event) {
@@ -44,6 +52,9 @@ function btnstartreset(event) {
         //speelveld_remove_click();
         start_spel = 0;
         this.innerHTML = 'Start spel';
+        gamerounds.innerHTML = 0;
+        player1score.innerHTML = 0;
+        player2score.innerHTML = 0;
         emptyspeelveld();
     }
 }
@@ -103,6 +114,9 @@ function check_winner(){
         imagelist[2].src.match('img/cross.jpg') && imagelist[4].src.match('img/cross.jpg') && imagelist[6].src.match('img/cross.jpg')
     ) {
         alert("Cross won!");
+        player1score.innerHTML = parseInt(player1score.innerHTML) + 1;
+        emptyspeelveld();
+        gamerounds.innerHTML = parseInt(gamerounds.innerHTML) + 1;
     } else if (
         imagelist[0].src.match('img/circle.jpg') && imagelist[1].src.match('img/circle.jpg') && imagelist[2].src.match('img/circle.jpg') ||
         imagelist[3].src.match('img/circle.jpg') && imagelist[4].src.match('img/circle.jpg') && imagelist[5].src.match('img/circle.jpg') ||
@@ -114,11 +128,18 @@ function check_winner(){
         imagelist[2].src.match('img/circle.jpg') && imagelist[4].src.match('img/circle.jpg') && imagelist[6].src.match('img/circle.jpg')
     ) {
         alert("Circle won!");
+        player2score.innerHTML = parseInt(player2score.innerHTML) + 1;
+        emptyspeelveld();
+        gamerounds.innerHTML = parseInt(gamerounds.innerHTML) + 1;
+
     } else if (
         !imagelist[0].src.match('img/empty.jpg') && !imagelist[1].src.match('img/empty.jpg') && !imagelist[2].src.match('img/empty.jpg') &&
         !imagelist[3].src.match('img/empty.jpg') && !imagelist[4].src.match('img/empty.jpg') && !imagelist[5].src.match('img/empty.jpg') &&
         !imagelist[6].src.match('img/empty.jpg') && !imagelist[7].src.match('img/empty.jpg') && !imagelist[8].src.match('img/empty.jpg')
     ){
         alert("It is a tie");
+        emptyspeelveld();
+        gamerounds.innerHTML = parseInt(gamerounds.innerHTML) + 1;
     }
 }
+
